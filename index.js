389b0e1ef5f74c249db87fe78a462a470af3ec54
Cherian Porter <cherianbelun@gmail.com>
2018-10-03 22:21:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "b97c0a57be78440bfb553bc4990aa212"
 
 function handleFormSubmit(event) {
   //handle submit event
+  event.preventDefault();
+  let city=document.querySelector('#city').value.replace(/ /g,'+');
+  
+  fetchCurrentWeather(city);
 }
 
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
-}
+  let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appi=${API_KEY}`
 
-function displayCurrentWeather(json) {
-  //render current weather data to the DOM using provided IDs and json from API
+  
+  
+  fetch(url)
+  .then(data => data.json())
+  .then(convertedData => displayCurrentWeather(convertedData)+console.log(convertedData));
+  
+
+function displayCurrentWeather(InputtedData) {
+  let temp= document.getElementById('temp');
+  let low= document.getElementById('low');
+  let high= document.getElementById('high');
+  let humidity=document.getElementById('hudmidity');
+  let sunrise=document.getElementById('sunrise');
+  let sunset = document.getElementById('sunset');
+  
+  temp.innerHTML = InputtedData.main.temp;
+  low.innerHTML = InputtedData.main.temp_min;
+  high.innerHTML = InputtedData.main.temp_max;
+  hudmidity.innerHTML =InputtedData.main.humidity;
+  
 }
 
 
@@ -27,4 +49,6 @@ function createChart(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
   //add event listener here for form submission
+  document.getElementById('cityform'); addEventListener('submit', handleFormSubmit);
+  document.getElementById('city').focus()
 })
